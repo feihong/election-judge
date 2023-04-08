0ad87db3fa2df911f480c15de4ccba0806687318
Feihong Hsu <feihong.hsu@gmail.com>
2023-04-08 14:30:07 -0500

    Generate web page instead of plain text

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Useful scripts for an election judge
 
+## Installation
+
+Install Python, then install dependencies:
+
+    pip install --requirement requirements.txt
+
 ## Directions
 
 1. Scan a PDF in Dropbox
 1. Copy the text and paste it into `judges.txt`
 1. Correct OCR errors
 1. Run `make`
+1. Console will say `Starting webserver at <ip address>:8000`
+1. Visit `<ip address>:8000` in your phone browser
+1. Tap each name under 'Inspection text messages' to send a text message to the given person
+1. Copy everything on the page except 'Inspection text messages' section to a new document in your Notes app
--- process.py
+++ process.py
@@ -1,8 +1,12 @@
 import re
 from pathlib import Path
+import socket
+import http.server
 from jinja2 import Template
 from settings import settings
 
+
+output_file = Path('index.html')
 
 def parse_text(text):
   name_lines = []
@@ -20,6 +24,7 @@ def parse_text(text):
 
 def get_names(lines):
   for line in lines:
+    # Reformat from "Smith, John A" to "John A Smith"
     parts = [n.capitalize() for n in re.split(r',?[ ]+', line.strip()) if n.strip()]
     family_name, *rest = parts
     yield ' '.join(rest + [family_name])
@@ -28,30 +33,78 @@ def get_numbers(lines):
   for line in lines:
     for part in line.split():
       yield part
+  
+def start_web_server():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.connect(('10.255.255.255', 1))
+  ip = s.getsockname()[0]
+
+  server_address = ('', 8000)
+  httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+  print(f'Starting webserver at {ip}:8000\n')
+  httpd.serve_forever()
+
 
 text = Path('judges.txt').read_text()
-items = list(parse_text(text))
-
-# Print contact list
-print('Contacts\n')
-for name, number in items:
-  print(f'{name}  {number}')
-
-print("""
+judges = list(parse_text(text))
+election_departments = [line.split('  ') for line in """\
 Election Central  312-269-7870
 Equipment & supplies  773-247-4065
 Polling Places Department  312-269-7976
-Registration Deparment  312-269-7960""")
+Registration Deparment  312-269-7960""".splitlines()]
 
-template = Template("""\
-Hi {{first_name}}, this is your {{role}}, {{subject_name}}. I plan to go to {{location}} on {{date}} at {{time}} to inspect \
-the voting equipment. Can you make it?""")
+html_template = Template("""\
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Election Info</title>
+</head>
+<body>
+<h1>Election Info</h1>
 
-print('\n\nText messages\n')
+<h2>Election Judges</h2>
+<pre>
+{% for name, number in judges -%}
+{{ name }}&nbsp; <a href="tel:{{ number }}">{{ number }}</a>
+{% endfor %}
+</pre>
 
-for name, number in items:
-  first, _rest = name.split(' ', 1)
-  settings['first_name'] = first
-  text = template.render(**settings)
-  print(text)
-  print(number + '\n')
+<h2>Election Departments</h2>
+<pre>
+{% for name, number in election_departments -%}
+{{ name }}&nbsp; <a href="tel:{{ number }}">{{ number }}</a>
+{% endfor %}
+</pre>
+
+<h2>Inspection text messages</h2>
+
+<ul>
+{% for name, number, mesg in text_messages %}
+  <li>
+    <a href="sms:{{ number }}?&body={{ mesg|urlencode }}">{{ name }}</a>
+  </li>
+{% endfor %}
+</ul>
+
+</body>
+""")
+
+text_message_template = Template("""\
+Hi {{first_name}}, this is your {{role}}, {{subject_name}}. I plan to go to {{location}} on {{date_time}} to inspect the voting equipment. Can you make it?""")
+
+def get_judges_with_text_messages(judges):
+  for name, number in judges:
+    if not number[0].isdigit():
+      continue
+    settings['first_name'] = name.split(' ', 1)[0]
+    mesg = text_message_template.render(**settings)
+    yield name, number, mesg
+
+with output_file.open('w') as fp:
+  html = html_template.render(
+    judges=judges,
+    election_departments=election_departments,
+    text_messages=get_judges_with_text_messages(judges))
+  fp.write(html)
+
+start_web_server()
